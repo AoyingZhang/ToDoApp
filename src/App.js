ad00c263be6cb4cd8b67279aff7d4127efac26de
AoyingZhang <zay20040811@gmail.com>
2023-05-27 18:39:12 -0400

    enabled adding item to the data file

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Header from "./Components/Header"
 import TaskList from "./Components/TaskList"
+import Data from "./Data"
 function App() {
+  const input = document.getElementById("inputEl")
   function appendElement(){
-
+    Data.push(`
+    {
+      Id:${Data.length+1}, 
+      Title: "${input.value}"
+    }
+    `)
+    input.value=""
   }
   return (
     <div className="App">
       <Header/>
-      <input type="text"/>
+      <input id="inputEl"type="text"/>
       <button className="addButton" onClick={appendElement}>Add</button>
       
       <TaskList/>
