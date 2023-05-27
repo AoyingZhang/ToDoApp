778e46f76d80dacbe49c4a72fc504f86189b90d4
AoyingZhang <zay20040811@gmail.com>
2023-05-27 14:42:19 -0400

    added checkbox

--- src/Components/Task.js
+++ src/Components/Task.js
@@ -1,9 +1,13 @@
 import React from "react"
 export default function Task(props){
     // Tasks contains title, description, color, status, Date
+    function completeTask(){
+        
+    }
     return (
-        <div>
-            <li className="task">{props.title}</li>
+        <div className="task">
+            <input type="checkbox" onChange={completeTask} {...props} />
+            <li >{props.title}</li>
 
         </div>
     )
